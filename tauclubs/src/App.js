c2d3b8844db43eb2c6a1f431711163ef895c3bb8
ceydabaloglu <e170503036@stud.tau.edu.tr>
2022-12-09 01:23:37 +0300

    Basic Homepage Try

--- tauclubs/src/App.js
+++ tauclubs/src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         TAUCLUBS
+        
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         
         </a>
       </header>
     </div>
