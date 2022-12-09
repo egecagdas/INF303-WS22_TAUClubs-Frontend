548572a4c0dd3fd22fe7693e7f9cb7da26b7b669
beyzagumuse <e190503031@stud.tau.edu.tr>
2022-12-09 20:51:44 +0300

    React101 Denemeleri - Önemsiz

--- tauclubs/src/App.js
+++ tauclubs/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const test = 34;
+    const isAuth = true;
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +20,23 @@ function App() {
         >
          
         </a>
+
+          <h1> Welcome to your Club! </h1>
+
+          <h2> {1+1} </h2>
+          <h2> { "murat".toUpperCase()}</h2>
+          <h2>{test + test}</h2>
+
+          <div>
+              {
+                  isAuth ? <p>Kullanıcı kayıtlı</p>
+                      :null
+              }
+          </div>
+
       </header>
+
+
     </div>
   );
 }
