882183e07009a254d95f508405a14c5d1f748abe
Suat Köroğlu <suatkoroglu06@gmail.com>
2022-12-19 21:03:57 +0300

    basics of login and register functions

--- tauclubs/src/page/Home.js
+++ tauclubs/src/page/Home.js
@@ -1,66 +1,62 @@
-import '../App.css';
-import logo from '../logo.svg';
-import User from '../components/user';
-import Navbar from '../components/Navbar';
+import "../App.css";
+import logo from "../logo.svg";
+import User from "../components/user";
+import Navbar from "../components/Navbar";
 //import Home from './components/Home'
 import Button from "@mui/material/Button";
-import BottomNavigation from '../components/BottomNavigation'
+import BottomNavigation from "../components/BottomNavigation";
+import Login from "../components/login";
 
 const Home = () => {
-
-    //const test = 34;
-    //const isAuth = true;
+  //const test = 34;
+  //const isAuth = true;
   return (
     <div>
       <header className="header">
-          <header className="card-header align-items-center p-5">
+        <header className="card-header align-items-center p-5">
+          <div className="navbar d-flex justify-content-between ">
+            <h1 className="sitename m-0">
+              <b>TAU</b>
+              <c>Clubs</c>
+            </h1>
+            <nav>
+              <ul className="d-flex">
+                <li>
+                  <a href="#Event">Event</a>
+                </li>
 
+                <li>
+                  <a href="Clubs">Clubs</a>
+                </li>
 
-              <div className="navbar d-flex justify-content-between ">
-                  <h1 className="sitename m-0">
-                    
-                    <b>TAU</b>
-                    <c>Clubs</c>
-                      
-                  </h1>
-                  <nav>
-                      <ul className="d-flex">
-                          <li><a href="#Event">Event</a></li>
-                          
-                          <li><a href="Clubs">Clubs</a></li>
-                          
-                          <li><a href="#Sign in">Sign in</a></li>
-                      </ul>
-                  </nav>
-              </div>
-
-
-          </header>
+                <li>
+                  <a href="#Sign in">Sign in</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
 
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-        </a>
-        <div className="d-flex justify-content-between align-items-center p-5 " >
+        ></a>
+        <div className="d-flex justify-content-between align-items-center p-5 ">
           <article>
-              
-              <p>Find your Club</p>
-              <p>Find  <t>yourself</t></p>  
-              
+            <p>Find your Club</p>
+            <p>
+              Find <t>yourself</t>
+            </p>
           </article>
-          <div> 
-                <button className="myButton">Join us</button>
-                <button className="myButton">Join </button>
+          <div>
+            <button className="myButton">Join us</button>
+            <button className="myButton">Join </button>
           </div>
-        </div>  
-        
+        </div>
       </header>
     </div>
   );
-}
+};
 export default Home;
-
-
