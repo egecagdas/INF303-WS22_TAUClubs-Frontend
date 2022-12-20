5f9be4db4865d951a7dd224f278ce161621a284b
Zeynep Sena Tatlı <zeynepsenatatli@gmail.com>
2022-12-20 09:07:49 +0300

    displaying posts of clubs on their own club pages

--- tauclubs/src/components/PostList.js
+++ tauclubs/src/components/PostList.js
@@ -1,48 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Post from "./Post";
 
-class PostList extends React.Component {
-  state = {
-    posts: [],
-  };
+// class PostList extends React.Component {
+//   state = {
+//     posts: [],
+//   };
 
-  componentDidMount() {
+//   componentDidMount() {
+//     let data;
+
+//     axios
+//       //   .get("http://localhost:8000/posts/?search=" + club_id + "/")
+//       .get("http://localhost:8000/posts/")
+
+//       .then((res) => {
+//         data = res.data;
+//         console.log(data);
+//         this.setState({
+//           posts: data,
+//         });
+//       })
+//       .catch((err) => {});
+//   }
+//   render() {
+//     // Destructing
+//     const { club_id, clubname } = this.props;
+
+function PostList(props) {
+  const { club_id, clubname } = props;
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
     let data;
 
     axios
-      //   .get("http://localhost:8000/posts/?search=" + club_id + "/")
-      .get("http://localhost:8000/posts/")
-
+      .get(`http://localhost:8000/posts/?search=${club_id}`)
       .then((res) => {
         data = res.data;
         console.log(data);
-        this.setState({
-          posts: data,
-        });
+        setPosts(data);
       })
       .catch((err) => {});
-  }
-  render() {
-    // Destructing
-    const { club_id, clubname } = this.props;
+  }, [club_id]);
 
-    return (
-      <div className="postList">
-        {this.state.posts.map((post) => (
-          <div>
-            <Post
-              category={post.type}
-              title={post.name}
-              content={post.description}
-              club={clubname}
-              time={post.postdate}
-            />
-          </div>
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="postList">
+      {/* {this.state.posts.map((post) => ( */}
+      {posts.map((post) => (
+        <div>
+          <Post
+            category={post.type}
+            title={post.name}
+            content={post.description}
+            club={clubname}
+            time={post.postdate}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default PostList;
