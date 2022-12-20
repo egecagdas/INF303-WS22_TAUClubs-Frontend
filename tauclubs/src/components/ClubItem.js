0caefab2d6eae06eb13fd3d955ee1ba8d84b47c1
Zeynep Sena Tatlı <zeynepsenatatli@gmail.com>
2022-12-20 08:17:36 +0300

    add redirect to the club's own page that is clicked on the clubs page

--- tauclubs/src/components/ClubItem.js
+++ tauclubs/src/components/ClubItem.js
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
-class ClubItem extends Component {
-  static defaultProps = {
-    club: "Bilgi Yok",
+// class ClubItem extends Component {
+function ClubItem(props) {
+  // static defaultProps = {
+  //   club: "Bilgi Yok",
+  // };
+
+  let navigate = useNavigate();
+  const goToClub = (club) => {
+    // this.props.history.push("/Clubs/" + club.id + "/");
+    // <Navigate to={`/Clubs/${club.id}`} />;
+    // this.props.navigation.navigate('/Clubs', paramsIfAny})
+
+    let path = `/Clubs/${club.id}`;
+    navigate(path);
   };
-  render() {
-    const { club } = this.props;
-    return (
-      <div class="my-3">
-        <div class="card w-auto">
-          <div class="card__body">
-            <h4>{club.name}</h4>
-            <p>CONTENT</p>
-          </div>
+
+  const { club } = props;
+  return (
+    <div class="my-3">
+      <div class="card w-auto" onClick={() => goToClub(club)}>
+        <div class="card__body">
+          <h4>{club.name}</h4>
+          <p>CONTENT</p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 ClubItem.propTypes = {
   club: PropTypes.object.isRequired,
+};
+
+ClubItem.defaultProps = {
+  club: "Bilgi Yok",
 };
 
 export default ClubItem;
