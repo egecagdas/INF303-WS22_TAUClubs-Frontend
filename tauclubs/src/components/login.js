14e227a0b9b2bf0285e70d92a38d96efcf5ddedf
Ege Cagdas Aladag <egecagdasaladag01@hotmail.com>
2022-12-22 13:53:35 +0300

    add login flow and save token to cookies

--- tauclubs/src/components/login.js
+++ tauclubs/src/components/login.js
@@ -1,31 +1,49 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
 
 class Login extends Component {
+  cookies = new Cookies();
   state = {
-    credentials: { email: "", password: "" },
+    credentials: { username: "", email: "", password: "" },
   };
 
-  // cookies = new Cookies();
   login = (event) => {
-    fetch("http://127.0.0.1:8000/dj-rest-auth/login/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(this.state.credentials),
-    })
-      .then(console.log(this.state.credentials))
-      .then((data) => data.json())
-      // .then((response) => {
-      //   // Save the authentication key in local storage
-      //   // localStorage.setItem("authKey", data.authKey);
-      //   // Redirect the user to the home page
-      //   // this.props.history.push("/");
-      //   console.log(response);
-      // })
-      // .then((data) => {
-      //   this.props.userLogin(data.token);
-      // })
-      .catch((error) => console.error(error));
+    axios
+      .post("http://127.0.0.1:8000/dj-rest-auth/login/", this.state.credentials)
+      .then((response) => {
+        console.log(response.data.key);
+        this.cookies.set("token", response.data.key);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
+  // Aşağıdaki kod gereksizse silebilirsiniz
+
+  // login = (event) => {
+  //   fetch("http://127.0.0.1:8000/dj-rest-auth/login/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(this.state.credentials),
+  //   })
+  //     .then(console.log(this.state.credentials))
+  //     .then((response) => {
+  //       console.log("here")
+  //     })
+  //     // .then((response) => {
+  //     //   // Save the authentication key in local storage
+  //     //   // localStorage.setItem("authKey", data.authKey);
+  //     //   // Redirect the user to the home page
+  //     //   // this.props.history.push("/");
+  //     //   console.log(response);
+  //     // })
+  //     // .then((data) => {
+  //     //   this.props.userLogin(data.token);
+  //     // })
+  //     .catch((error) => console.error(error));
+  // };
 
   inputChanged = (event) => {
     const cred = this.state.credentials;
